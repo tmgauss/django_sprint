c3228435dd1a28bfb6cc4341a87770774c02bd42
tmgauss <steveforever7822@gmail.com>
2020-05-05 17:45:43 +0900

    Add top page

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class TopView(TemplateView):
+    template_name = 'cms/top.html'
